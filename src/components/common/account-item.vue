b3cdb469a3259b5dd143b3834780561ab3e6d6da
garcia-alejandro <alejandro@hashed.io>
2022-08-05 15:49:52 -0500

    Updated the component

--- src/components/common/account-item.vue
+++ src/components/common/account-item.vue
@@ -3,7 +3,9 @@ q-card(:flat="flat" :class="{'inherit': inherit}" :bordered="bordered")
   q-item
     q-item-section(avatar)
       account-icon(:address="address" size="2em")
-    q-item-section {{ displayName }}
+    q-item-section
+      q-item-label(lines="1") {{ displayName }}
+    slot
 </template>
 
 <script>
--- src/components/common/accounts-menu.vue
+++ src/components/common/accounts-menu.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 q-menu(fit auto-close)
     q-list
-        q-item(clickable v-for="account in accounts" @click="selectAccount(account)" :class="[isSelected(account) ? 'bg-primary text-white text-bold' : 'label', 'dropdown-item']")
+        q-item(clickable v-for="account in accounts" @click="selectAccount(account)" :class="[isSelected(account) ? 'bg-primary text-white text-bold' : 'text-primary']")
           q-item-section(avatar)
             account-icon(:address="account.address" size="1.8em")
           q-item-section
@@ -12,14 +12,23 @@ q-menu(fit auto-close)
 
 <script>
 import AccountIcon from '~/components/common/account-icon.vue'
+/**
+ * Component used to show menu of available accounts
+ */
 export default {
   name: 'AccountsMenu',
   components: { AccountIcon },
   props: {
+    /**
+     * Accounts address
+     */
     accounts: {
       type: Array,
       default: () => []
     },
+    /**
+     * Object with selected account info
+     */
     selectedAccount: {
       type: Object,
       default: () => {}
@@ -33,6 +42,9 @@ export default {
       return account.address === this.selectedAccount.address
     },
     selectAccount (account) {
+      /**
+       * Emit data when an account is selected
+       */
       this.$emit('selectAccount', account)
     }
   }
