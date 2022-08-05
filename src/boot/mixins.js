ed2d36de3b1570201808e7a819d9846d626f20e6
garcia-alejandro <alejandro@hashed.io>
2022-08-05 15:49:14 -0500

    Added h-input globally

--- src/boot/mixins.js
+++ src/boot/mixins.js
@@ -2,9 +2,12 @@
 // import Vue from 'vue'
 import notifications from '~/mixins/notifications'
 import TInput from '~/components/common/input/t-input.vue'
+import HInput from '~/components/common/input/h-input.vue'
 // import { validation } from '~/mixins/validation'
 export default async ({ app }) => {
   app.mixin(notifications)
   app.component('TInput', TInput)
+  app.component('HInput', HInput)
+
   // app.mixin(validation)
 }
